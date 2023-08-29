c941bb7d8ae134fecc76ed61b96ebe2c926531b0
3KR4 <mouhamedmahmoud820@gmail.com>
2023-08-29 15:57:08 +0200

    goku

--- js/blogs-grid.js
+++ js/blogs-grid.js
@@ -27,17 +27,19 @@ let blogCards = document.querySelector(".blogs .cards")
 function rednderblogs() {
 let blogItems = blogs.map((item) => {
   return`
-  <div class="card col-12 col-lg-4 col-md-6">
-  <div class="img-holder">
-    <img src="${item.img}" alt="">
-    <div class="animation"><a href="#">Read More</a></div>
-  </div>
-  <div class="info">
-    <div class="users">
-      <p><i class="fa-regular fa-user"></i> Musharof Chy</p>
-      <p><i class="fa-regular fa-file-lines"></i> 25 Dec, 2025</p>
+  <div class="col-sm-12 col-lg-4 col-md-6">
+    <div class="card">
+    <div class="img-holder">
+      <img src="${item.img}" alt="">
+      <div class="animation"><a href="#">Read More</a></div>
     </div>
-    <h3>Free advertising for your online business</h3>
+    <div class="info">
+      <div class="users">
+        <p><i class="fa-regular fa-user"></i> Musharof Chy</p>
+        <p><i class="fa-regular fa-file-lines"></i> 25 Dec, 2025</p>
+      </div>
+      <h3>Free advertising for your online business</h3>
+    </div>
   </div>
 </div>
   `
