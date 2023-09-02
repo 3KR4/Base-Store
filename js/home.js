28491d6cbfb632d916f82fc373d1ca52512fdfdf
3KR4 <mouhamedmahmoud820@gmail.com>
2023-09-02 03:20:45 +0200

    luffy

--- js/home.js
+++ js/home.js
@@ -1,130 +1,3 @@
-                                                            //! testimonials
-const testimonials = [
-
-  {
-    id:1, 
-    mainImg:"img/avatar/avatar 1.png",
-    name:"Eren Yeager",
-    year:2013,
-    anime:"Attack On Titan",
-    img:"img/dropcom.png"
-  },
-  {
-    id:2, 
-    mainImg:"img/avatar/avatar 2.jpeg",
-    name:"Monkey D. Luffy",
-    year:1999,
-    anime:"One Piece",
-    img:"img/dropcom.png"
-  },
-  {
-    id:3,
-    mainImg:"img/avatar/avatar 3.jpeg", 
-    name:"Naruto",
-    year:2000,
-    anime:"Naruto hippuden",
-    img:"img/dropcom.png"
-  },
-  {
-    id:4, 
-    mainImg:"img/avatar/avatar 4.jpeg",
-    name:"Gojo Satoru",
-    year:2020,
-    anime:"jujutsu kaisen",
-    img:"img/dropcom.png"
-  },
-  {
-    id:5, 
-    mainImg:"img/avatar/avatar 5.jpeg",
-    name:"Senku",
-    year:2019,
-    anime:"Dr.Stone",
-    img:"img/dropcom.png"
-  },
-  {
-    id:6, 
-    mainImg:"img/avatar/avatar 6.jpeg",
-    name:"Asta",
-    year:2016,
-    anime:"Black Clover",
-    img:"img/dropcom.png"
-  },
-  {
-    id:7,
-    mainImg:"img/avatar/avatar 7.jpeg",
-    name:"Thorfinn",
-    year:2019,
-    anime:"Vinland Saga",
-    img:"img/dropcom.png"
-  }
-  ];
-  let testCards = document.querySelector(".testimonials .cards .mySwiper  .swiper-wrapper ")
-  function rednderTestimonials() {
-  let testItems = testimonials.map((item) => {
-  
-    return`
-    
-    <div class="swiper-slide">
-      <div class="card">
-      <img src="${item.mainImg}" alt="">
-      <div class="text-holder">
-        <h1>${item.name}</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Vero ut voluptates optio, delectus amet illo reiciendis tempore, 
-          odio cum inventore sapiente consequuntur natus a ab sunt 
-          culpa dolorem veritatis hic.</p>
-        <div class="holder">
-          <div class="info">
-            <h5>${item.year}</h5>  
-            <p>${item.anime}</p>
-          </div>
-          <img class="logo" src="${item.img}" alt="">
-        </div>
-      </div>
-    </div>
-    </div>
-    `
-  }) 
-  testCards.innerHTML = testItems
-  };
-  rednderTestimonials();
-
-                                                            //! blogs
-
-const blogs = [ 
-{
-  img:"img/blog-01.png"
-},
-{
-  img:"img/blog-02.png"
-},
-{
-  img:"img/blog-03.png"
-},
-]
-let blogCards = document.querySelector(".blogs .cards")
-function rednderblogs() {
-let blogItems = blogs.map((item) => {
-  return`
-  <div class="card col-12 col-lg-4 col-md-6">
-  <div class="img-holder">
-    <img src="${item.img}" alt="">
-    <div class="animation"><a href="#">Read More</a></div>
-  </div>
-  <div class="info">
-    <div class="users">
-      <p><i class="fa-regular fa-user"></i> Musharof Chy</p>
-      <p><i class="fa-regular fa-file-lines"></i> 25 Dec, 2025</p>
-    </div>
-    <h3>Free advertising for your online business</h3>
-  </div>
-</div>
-  `
-}).join("") 
-blogCards.innerHTML = blogItems
-}
-rednderblogs();
-
                                                             //! checkbox
 function check() {
   const checkbox = document.querySelector(".checkbox")
@@ -141,7 +14,6 @@ function check() {
       pricethere.innerHTML = "<div><h3>$139</h3>/per month</div>" 
     };
 }
-
                                                             //! Filter
 
 const btns = [
@@ -317,4 +189,151 @@ function manageFilter() {
     el.style.display = "block";
   });
 }
+                                                            //! testimonials
+const testimonials = [
 
+  {
+    id:1, 
+    mainImg:"img/avatar/avatar 1.png",
+    name:"Eren Yeager",
+    year:2013,
+    anime:"Attack On Titan",
+    img:"img/dropcom.png"
+  },
+  {
+    id:2, 
+    mainImg:"img/avatar/avatar 2.jpeg",
+    name:"Monkey D. Luffy",
+    year:1999,
+    anime:"One Piece",
+    img:"img/dropcom.png"
+  },
+  {
+    id:3,
+    mainImg:"img/avatar/avatar 3.jpeg", 
+    name:"Naruto",
+    year:2000,
+    anime:"Naruto hippuden",
+    img:"img/dropcom.png"
+  },
+  {
+    id:4, 
+    mainImg:"img/avatar/avatar 4.jpeg",
+    name:"Gojo Satoru",
+    year:2020,
+    anime:"jujutsu kaisen",
+    img:"img/dropcom.png"
+  },
+  {
+    id:5, 
+    mainImg:"img/avatar/avatar 5.jpeg",
+    name:"Senku",
+    year:2019,
+    anime:"Dr.Stone",
+    img:"img/dropcom.png"
+  },
+  {
+    id:6, 
+    mainImg:"img/avatar/avatar 6.jpeg",
+    name:"Asta",
+    year:2016,
+    anime:"Black Clover",
+    img:"img/dropcom.png"
+  },
+  {
+    id:7,
+    mainImg:"img/avatar/avatar 7.jpeg",
+    name:"Thorfinn",
+    year:2019,
+    anime:"Vinland Saga",
+    img:"img/dropcom.png"
+  }
+  ];
+  let testCards = document.querySelector(".testimonials .cards .mySwiper  .swiper-wrapper ")
+  function rednderTestimonials() {
+  let testItems = testimonials.map((item) => {
+  
+    return`
+    
+    <div class="swiper-slide">
+      <div class="card">
+      <img src="${item.mainImg}" alt="">
+      <div class="text-holder">
+        <h1>${item.name}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Vero ut voluptates optio, delectus amet illo reiciendis tempore, 
+          odio cum inventore sapiente consequuntur natus a ab sunt 
+          culpa dolorem veritatis hic.</p>
+        <div class="holder">
+          <div class="info">
+            <h5>${item.year}</h5>  
+            <p>${item.anime}</p>
+          </div>
+          <img class="logo" src="${item.img}" alt="">
+        </div>
+      </div>
+    </div>
+    </div>
+    `
+  }) 
+  testCards.innerHTML = testItems
+  };
+  rednderTestimonials();
+                                                            //! blogs
+
+const blogs = [ 
+{
+  img:"img/blog-01.png"
+},
+{
+  img:"img/blog-02.png"
+},
+{
+  img:"img/blog-03.png"
+},
+]
+let blogCards = document.querySelector(".blogs .cards")
+function rednderblogs() {
+let blogItems = blogs.map((item) => {
+  return`
+  <div class="card col-12 col-lg-4 col-md-6">
+  <div class="img-holder">
+    <img src="${item.img}" alt="">
+    <div class="animation"><a href="#">Read More</a></div>
+  </div>
+  <div class="info">
+    <div class="users">
+      <p><i class="fa-regular fa-user"></i> Musharof Chy</p>
+      <p><i class="fa-regular fa-file-lines"></i> 25 Dec, 2025</p>
+    </div>
+    <h3>Free advertising for your online business</h3>
+  </div>
+</div>
+  `
+}).join("") 
+blogCards.innerHTML = blogItems
+}
+rednderblogs();
+                                                            //! States
+let nums = document.querySelectorAll(".num")
+let states = document.querySelector(".states") 
+let started = false;
+
+window.onscroll = function () {
+  if (window.scrollY >= states.offsetTop - 900) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++
+    if (el.textContent == goal) {
+      clearInterval(count)
+    }
+  }, 2000 / goal);
+};
