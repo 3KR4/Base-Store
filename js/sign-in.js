ddb8a28e636fe71cb51afb520082805d29edcd42
3KR4 <mouhamedmahmoud820@gmail.com>
2023-09-14 16:55:37 +0200

    final

--- js/sign-in.js
+++ js/sign-in.js
@@ -1,6 +1,19 @@
 const form = document.querySelector("form"),
+  nameField = form.querySelector(".name-field"),
+  nameInput = nameField.querySelector(".name"),
   passField = form.querySelector(".create-password"),
   passInput = passField.querySelector(".password");
+
+
+
+// user name Validtion
+function checkName() {
+  if (!nameInput.value !== "" && nameInput.value.length < 4) {
+    return nameField.classList.add("invalid"); //adding invalid class if email value do not mathced with email pattern
+  }
+  nameField.classList.remove("invalid"); //removing invalid class if email value matched with emaiPattern
+  }
+
 
 const eye = document.querySelectorAll(".show-hide")
 eye.forEach((eye) => {
@@ -20,7 +33,6 @@ eye.forEach((eye) => {
 function createPass() {
   const passPattern =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
   if (!passInput.value.match(passPattern)) {
     return passField.classList.add("invalid"); //adding invalid class if password input value do not match with passPattern
   }
@@ -31,18 +43,23 @@ function createPass() {
 // Calling Funtion on Form Sumbit
 form.addEventListener("submit", (e) => {
   e.preventDefault(); //preventing form submitting
+  checkName();
   createPass();
 
 
   //calling function on key up
+  nameInput.addEventListener("keyup", checkName);
   passInput.addEventListener("keyup", createPass);
 
   if (
+    !nameField.classList.contains("invalid") &&
     !passField.classList.contains("invalid")
   ) {
     location.href = form.getAttribute("action");
   }
 });
+
+
 
 
 
--- js/sign-up.js
+++ js/sign-up.js
@@ -1,10 +1,24 @@
 const form = document.querySelector("form"),
+  nameField = form.querySelector(".name-field"),
+  nameInput = nameField.querySelector(".name"),
   emailField = form.querySelector(".email-field"),
   emailInput = emailField.querySelector(".email"),
   passField = form.querySelector(".create-password"),
   passInput = passField.querySelector(".password"),
   cPassField = form.querySelector(".confirm-password"),
   cPassInput = cPassField.querySelector(".cPassword");
+
+
+
+
+
+// user name Validtion
+function checkName() {
+  if (!nameInput.value !== "" && nameInput.value.length < 4) {
+    return nameField.classList.add("invalid"); //adding invalid class if email value do not mathced with email pattern
+  }
+  nameField.classList.remove("invalid"); //removing invalid class if email value matched with emaiPattern
+}
 
 // Email Validtion
 function checkEmail() {
@@ -14,20 +28,6 @@ function checkEmail() {
   }
   emailField.classList.remove("invalid"); //removing invalid class if email value matched with emaiPattern
 }
-
-const eye = document.querySelectorAll(".show-hide")
-eye.forEach((eye) => {
-  eye.addEventListener("click" , () => {
-    const passInbut = eye.parentElement.querySelector("input");
-    if (passInbut.type === "password") {
-      eye.classList.replace("bx-hide", "bx-show")
-      return (passInbut.type = "text");
-    }else {
-      eye.classList.replace("bx-show", "bx-hide");
-      passInbut.type = "password"
-    };
-  });
-});
 
 // Password Validation
 function createPass() {
@@ -48,63 +48,42 @@ function confirmPass() {
   cPassField.classList.remove("invalid");
 }
 
+const eye = document.querySelectorAll(".show-hide")
+eye.forEach((eye) => {
+  eye.addEventListener("click" , () => {
+    const passInbut = eye.parentElement.querySelector("input");
+    if (passInbut.type === "password") {
+      eye.classList.replace("bx-hide", "bx-show")
+      return (passInbut.type = "text");
+    }else {
+      eye.classList.replace("bx-show", "bx-hide");
+      passInbut.type = "password"
+    };
+  });
+});
+
+
 // Calling Funtion on Form Sumbit
 form.addEventListener("submit", (e) => {
   e.preventDefault(); //preventing form submitting
+  checkName();
   checkEmail();
   createPass();
   confirmPass();
 
   //calling function on key up
+  nameInput.addEventListener("keyup", checkName);
   emailInput.addEventListener("keyup", checkEmail);
   passInput.addEventListener("keyup", createPass);
   cPassInput.addEventListener("keyup", confirmPass);
 
   if (
+    !nameField.classList.contains("invalid") &&
     !emailField.classList.contains("invalid") &&
     !passField.classList.contains("invalid") &&
     !cPassField.classList.contains("invalid")
+
   ) {
     location.href = form.getAttribute("action");
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
